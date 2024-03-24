484c705fc58a1a4941d654d564a51fda9920b9b0
Soriyama <ecalorodriguez@gmail.com>
2024-03-24 20:52:24 +0100

    Comprobación de botones

--- PROYECTO_NYE_RIEGO/PROYECTO_NYE_RIEGO.ino
+++ PROYECTO_NYE_RIEGO/PROYECTO_NYE_RIEGO.ino
@@ -23,15 +23,15 @@ Adafruit_BMP280 bmp;
 const char *ssid = "Tarta-D";
 const char *password = "FUFU2022...";
 // PINES DE ENTRADA
-const int PINES_ENTRADA[] = {14, 15, 13, 32, 35, 39};
+const int PINES_ENTRADA[] = {14, 15, 12, 32, 26, 22};
 // ENUMERACIÓN PARA INDEXAR LOS PINES DE ENTRADA
 enum IndiceEntrada {
   ARRIBA,
   ABAJO,
   IZQUIERDA,
   DERECHA,
-  BOTON_EXTERIOR,
-  BOTON_INTERIOR
+  BOTON_INTERIOR,
+  BOTON_EXTERIOR
 };
 // PINES DE SALIDA
 const int PINES_SALIDA[] = {21, 19, 18, 5};
@@ -43,7 +43,10 @@ enum IndiceSalida {
   LIBRE_2
 };
 // GLOBALES
+unsigned long ultimoTiempo = 0;
+const unsigned long intervaloLectura = 2000;  // Intervalo de tiempo entre lecturas en milisegundos
 
+// FUNCIONES
 void setup() {
   Serial.begin(115200);
   // Inicializar la comunicación I2C con los pines SDA y SCL especificados
@@ -74,38 +77,83 @@ void setup() {
 
   // Especificación del PIN como entrada o salida
   pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(PINES_ENTRADA[ARRIBA], INPUT_PULLDOWN);
+  pinMode(PINES_ENTRADA[ABAJO], INPUT_PULLDOWN);
+  pinMode(PINES_ENTRADA[IZQUIERDA], INPUT_PULLDOWN);
+  pinMode(PINES_ENTRADA[DERECHA], INPUT_PULLDOWN);
+  pinMode(PINES_ENTRADA[BOTON_EXTERIOR], INPUT_PULLDOWN);
+  pinMode(PINES_ENTRADA[BOTON_INTERIOR], INPUT_PULLDOWN);
+  pinMode(PINES_SALIDA[RIEGO_EXTERIOR], OUTPUT);
+  pinMode(PINES_SALIDA[RIEGO_INTERIOR], OUTPUT);
+  pinMode(PINES_SALIDA[LIBRE_1], OUTPUT);
+  pinMode(PINES_SALIDA[LIBRE_2], OUTPUT);
+  // SE APAGAN LAS SALIDAS
+  digitalWrite(PINES_SALIDA[RIEGO_EXTERIOR], LOW);
+  digitalWrite(PINES_SALIDA[RIEGO_INTERIOR], LOW);
+  digitalWrite(PINES_SALIDA[LIBRE_1], LOW);
+  digitalWrite(PINES_SALIDA[LIBRE_2], LOW);
+  // SE MUESTRA UN MENSAJE DE BIENVENIDA
+  imprimirMensaje("Iniciando sistema...");
+  delay(2000);
+  lcd.clear();
 }
+
 
 void loop() {
-  // Lee el estado del botón de reset
-  int estadoBoton = digitalRead(RESET_BUTTON_PIN);
+  unsigned long tiempoActual = millis();
 
-  // Si el botón de reset está presionado (estado bajo)
-  if (estadoBoton == LOW) {
-    Serial.println("Boton de reset presionado. Reiniciando...");
-    delay(100); // Debounce - espera un breve periodo para evitar rebotes
-    ESP.restart(); // Reinicia la placa ESP32
+  // Realizar la lectura solo si ha pasado el intervalo de tiempo
+  if (tiempoActual - ultimoTiempo >= intervaloLectura) {
+    ultimoTiempo = tiempoActual; // Actualizar el tiempo de la última lectura
+
+    // Lee el estado del botón de reset
+    int estadoBoton = digitalRead(RESET_BUTTON_PIN);
+
+    // Si el botón de reset está presionado (estado bajo)
+    if (estadoBoton == LOW) {
+      Serial.println("Boton de reset presionado. Reiniciando...");
+      ESP.restart(); // Reinicia la placa ESP32
+    }
+
+    // Imprimir lecturas de temperatura
+    lcd.setCursor(12, 0);
+    lcd.print(bmp.readTemperature());
+    lcd.print(" C");
   }
 
-  // Imprimir lecturas de temperatura, presión y altitud
-  lcd.setCursor(0, 0);
-  lcd.print("ESTACION");
+  // Realizar la lectura de los botones
+  int estadoBotonArriba = digitalRead(PINES_ENTRADA[ARRIBA]);
+  int estadoBotonAbajo = digitalRead(PINES_ENTRADA[ABAJO]);
+  int estadoBotonIzquierda = digitalRead(PINES_ENTRADA[IZQUIERDA]);
+  int estadoBotonDerecha = digitalRead(PINES_ENTRADA[DERECHA]);
+  int estadoBotonExterior = digitalRead(PINES_ENTRADA[BOTON_EXTERIOR]);
+  int estadoBotonInterior = digitalRead(PINES_ENTRADA[BOTON_INTERIOR]);
 
-  lcd.setCursor(0, 1);
-  lcd.print("Temp =");
-  lcd.print(bmp.readTemperature());
-  lcd.print(" *C");
-
-  lcd.setCursor(0, 2);
-  lcd.print("Press = ");
-  lcd.print(bmp.readPressure());
-  lcd.print(" Pa");
-
-  lcd.setCursor(0, 3);
-  lcd.print("ALT = ");
-  lcd.print(bmp.readAltitude(1013.25)); // Ajusta esta presión al nivel del mar local
-  lcd.print(" m");
-
-  delay(2000);
+  // Si el botón de arriba está presionado
+  if (estadoBotonArriba == HIGH) {
+    lcd.clear();
+    imprimirMensaje("Arriba presionado");
+  }else if (estadoBotonAbajo == HIGH) {
+    lcd.clear();
+    imprimirMensaje("Abajo presionado");
+  }else if (estadoBotonIzquierda == HIGH) {
+    imprimirMensaje("Izquierda presionado");
+  }else if (estadoBotonDerecha == HIGH) {
+    lcd.clear();
+    imprimirMensaje("Derecha presionado");
+  }else if (estadoBotonExterior == HIGH) {
+    lcd.clear();
+    imprimirMensaje("Exterior presionado");
+  }else if (estadoBotonInterior == HIGH) {
+    lcd.clear();
+    imprimirMensaje("Interior presionado");
+  }else {
+    // imprimirMensaje("Esperando...");
+  }
 }
 
+void imprimirMensaje(String mensaje) {
+  lcd.setCursor(0, 1);
+  lcd.print(mensaje);
+  }
+ 
